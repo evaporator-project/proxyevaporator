74b38b574de6d050651301f28d1995e5ad1cd73e
tzhangm <tzhangm@trip.com>
2022-12-04 16:02:49 +0800

    feat: add cors

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import axios from 'axios'
 import bodyParser from 'body-parser'
+// @ts-ignore
+import cors from 'cors'
 const app = express()
+app.use(cors())
 app.use(bodyParser.json());
 
 app.use(express.static('public'))
